e8c8a82ec6751ea507674dcf1449650c84dc0c46
Clement Colin <ccolin@c1r3s2.42tokyo.jp>
2024-09-08 17:04:17 +0900

    Quick commit

--- Makefile
+++ Makefile
@@ -6,14 +6,14 @@
 #    By: ccolin <ccolin@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/08/08 16:19:44 by ccolin            #+#    #+#              #
-#    Updated: 2024/08/23 15:58:25 by ccolin           ###   ########.fr        #
+#    Updated: 2024/09/08 17:00:40 by ccolin           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME	= so_long
 
 CC		= cc
-CFLAGS	= -Wall -Wextra -Werror -Iincludes -fsanitize=address
+CFLAGS	= -Wall -Wextra -Werror -Iincludes #-fsanitize=address
 
 MLX_DIR	= minilibx_opengl_20191021
 MLX_LIB	= $(MLX_DIR)/libmlx.a
--- src/main.c
+++ src/main.c
@@ -6,11 +6,18 @@
 /*   By: ccolin <ccolin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 11:06:16 by ccolin            #+#    #+#             */
-/*   Updated: 2024/09/08 16:30:39 by ccolin           ###   ########.fr       */
+/*   Updated: 2024/09/08 17:03:42 by ccolin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+// #include <libc.h>
+// __attribute__((destructor))
+// static void destructor() {
+//     system("leaks -q so_long");
+// }
+// system("leaks a.out");
 
 int	main(int argc, char	**argv)
 {
@@ -21,7 +28,6 @@ int	main(int argc, char	**argv)
 		ft_error("Usage: ./so_long filename.ber\n");
 	extractmap(argv[1], &map);
 	ft_start_mlx(&map, &mlx);
-	system("leaks a.out");
 	mlx.moves = 0;
 	mlx_loop(mlx.mlx);
 }
